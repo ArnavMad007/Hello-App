111ba86572be674390e05936a9b8b87dd9226353
ArnavMad007 <arnavmadkaikar@gmail.com>
2026-04-10 18:20:17 +0530

    Adding uc1 file

---
+++ Hello-App.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App
+ * UC1 - Display Hello World
+ * @author Arnav.M
+ * @version 1.0
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
