eab67258e8fae73493fdd62d51f5c80d20bd3a64
ArnavMad007 <arnavmadkaikar@gmail.com>
2026-04-10 18:39:17 +0530

    Initial commit

---
+++ Hello-App/Hello-App.java
@@ -0,0 +1,20 @@
+/**
+ * OOPS Banner App
+ * UC2 - Display Hello with Command-Line Argument
+ * @author Arnav.M
+ * @version 2.0
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            
+            System.out.println("Hello, World!");
+        }
+    }
